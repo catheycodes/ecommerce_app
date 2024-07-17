d6f0a583f84551f56622e40368e30dca445224fb
catheycodes <168264242+catheycodes@users.noreply.github.com>
2024-07-17 12:44:46 +0530

    Update HomePage.dart

--- lib/HomeScreen/HomePage.dart
+++ lib/HomeScreen/HomePage.dart
@@ -48,7 +48,7 @@ class _HomePageState extends State<HomePage> {
                   color: Colors.grey.withOpacity(0.5),
                   spreadRadius: 2,
                   blurRadius: 5,
-                  offset: const Offset(0, 3), // changes position of shadow
+                  offset: const Offset(0, 3), 
                 ),
               ],
             ),
